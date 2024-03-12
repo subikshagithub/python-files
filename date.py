27cba517bf2ed23525202377cd171d324223b077
subikshagithub <159299970+subikshagithub@users.noreply.github.com>
2024-03-12 23:11:11 +0530

    Add files via upload

---
+++ date.py
@@ -0,0 +1,54 @@
+#  Date Time in Python
+
+# import datetime as dt
+
+# current_date=dt.date.today()
+# print(current_date)
+# new=dt.date(2024,3,10)
+# print(new)
+# print("Year:",new.year)
+# print("Month:",new.month)
+# print("Day:",new.day)
+
+# --------------------
+
+# a=dt.time(12,45,20,40444)
+# print(a)
+# print("Hour:",a.hour)
+# print("Minute:",a.minute)
+# print("Second:",a.second)
+# print("Microsecond:",a.microsecond)
+
+
+# --------------------
+# a=dt.datetime.now()
+# print("Current Time:",a)
+
+# --------------------
+
+# a=dt.datetime(2024,5,20,10,33,25)
+# print(a)
+# print(a.date())
+# print(a.time())
+
+# --------------------
+
+# current_day=dt.datetime.now()
+# new_year=dt.datetime(2025,1,1)
+# diffrence= new_year - current_day
+# print(diffrence)
+
+# --------------------
+
+# current=dt.datetime.now()
+# print(current)
+
+# a=current.strftime("%A %B %D %Y")
+# print(a)
+
+# --------------------
+# current=dt.datetime.now()
+# print(current)
+
+# a=current.strftime("%a %b %d %Y")
+# print(a)
